7904c68406905b18fa7491dce00dc1e79a6e57f4
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2024-02-10 04:31:14 +0100

    first steps - Creating base templates

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+users_list = ["Tiago", "Robert", "Ana", "Vanessa", "Marcus", "Andi"]
+
 @app.route("/")
 def hello_world():
-    return "VAMOS COMECAR!!!</p>"
+    return render_template("home.html")
 
-@app.route("/teste")
-def teste():
-    return "Vamos ver se vai dar certo!"
 
-@app.route("/mais")
-def mais():
-    return "Mais Uma tentativa"
+@app.route("/users")
+def users():
+    return render_template("users.html", users_list=users_list)
+
 
 
 if __name__ == "__main__":
